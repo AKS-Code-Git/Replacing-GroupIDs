772eb7f46b7fa2c41d17ec79e3b95f08fa4e1536
AKS-Code-Git <74342692+AKS-Code-Git@users.noreply.github.com>
2023-01-19 20:17:03 +0530

    updated

--- src/main/java/com/crud/ctl/DomaineventsController.java
+++ src/main/java/com/crud/ctl/DomaineventsController.java
@@ -29,35 +29,39 @@ public class DomaineventsController {
 	private MongoOperations mongoOperation;
 
 	/**
-	 * @param oldID GroupID of new netting service
-	 * @param newID GroupID of old netting service
+	 * Replaces ID field in 'serializedPayload' in all the documents.
+	 * 
+	 * @param oldID GroupID of new netting service.
+	 * @param newID GroupID of old netting service.
 	 * @return
 	 */
 	@GetMapping("/setNewGID/{oldID}/{newID}")
 	@ResponseBody
-	public List<Domainevents> updateGroupID(@PathVariable String oldID, @PathVariable String newID) {
-		
-
-		Map<String, String> ab = new HashMap<String, String>();
+	public String updateGroupID(@PathVariable String oldID, @PathVariable String newID) {
+//		 List<Domainevents>
+		final Map<String, String> ab = new HashMap<String, String>();
 		Query query = new Query();
 //		final String criteria = "groupCreated\":"; // this can be replaced with 'oldID'
 		query.addCriteria(Criteria.where("serializedPayload").regex(oldID));
 		List<Domainevents> dm = mongoOperation.find(query, Domainevents.class);
-		
+
 		System.out.println("Total documents found : " + dm.size());
+		
+		final StringBuilder sb = new StringBuilder();
 		for (Iterator<Domainevents> iterator = dm.iterator(); iterator.hasNext();) {
 			Domainevents domainevents = iterator.next();
 			String s1 = getGroupID(domainevents.getSerializedPayload());
 			ab.put(domainevents.get_id(), s1);
+			sb.append(domainevents.get_id() + ", ");
 		}
-		List<String> ids = getRecID(oldID, ab);
-
+		final List<String> ids = getRecID(oldID, ab);
+		String string;
 		for (Iterator<String> iterator = ids.iterator(); iterator.hasNext();) {
-			String string = (String) iterator.next();
+			string = iterator.next();
 			updateIDs(string, oldID, newID);
 		}
-
-		return dm;
+		
+		return "Total documents found/updated : " + dm.size() +" & _ID's are : "+ sb.toString();
 	}
 
 	/**
@@ -65,12 +69,12 @@ public class DomaineventsController {
 	 * @param oldID
 	 * @param newID
 	 */
-	private void updateIDs(String recID, String oldID, String newID) {
+	private void updateIDs(final String recID, final String oldID, final String newID) {
 
 		Query query = new Query();
 		query.addCriteria(Criteria.where("_id").is(recID));
 		List<Domainevents> dm = mongoOperation.find(query, Domainevents.class);
-		System.out.println(dm.get(0).get_id() + " :\n dm : " + dm.size() + "\n : " + dm.get(0).getSerializedPayload());
+//		System.out.println(dm.get(0).get_id() + " :\n dm : " + dm.size() + "\n : " + dm.get(0).getSerializedPayload());
 		final String spl = dm.get(0).getSerializedPayload();
 		final String spl_new = spl.replaceAll(oldID, newID);
 
@@ -84,10 +88,12 @@ public class DomaineventsController {
 	}
 
 	/**
+	 * Fetches out the groupID from 'serializedPayload' through string operations.
+	 * 
 	 * @param serializedPayload
-	 * @return
+	 * @return GroupID.
 	 */
-	private String getGroupID(String serializedPayload) {
+	private String getGroupID(final String serializedPayload) {
 		int index = serializedPayload.indexOf("\\\"id\\\":");
 		String start = serializedPayload.substring(index);
 		start = start.substring(0, start.indexOf(",")).replaceAll("\"", "").replaceAll("id", "").replaceAll("\\\\:", "")
@@ -97,16 +103,18 @@ public class DomaineventsController {
 	}
 
 	/**
-	 * @param id
+	 * Creates list of all '_id' where replacements are required.
+	 * 
+	 * @param groupIDtoReplace
 	 * @param ab
-	 * @return
+	 * @return List of all '_id' where replacements are required.
 	 */
-	private List<String> getRecID(String id, Map<String, String> ab) {
+	private List<String> getRecID(String groupIDtoReplace, Map<String, String> ab) {
 		final List<String> keys = new ArrayList<String>();
 		final Iterator<String> itr = ab.keySet().iterator();
 		while (itr.hasNext()) {
 			String key = itr.next();
-			if (ab.get(key).equals(id)) {
+			if (ab.get(key).equals(groupIDtoReplace)) {
 				keys.add(key);
 			}
 		}
